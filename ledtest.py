c52b6f2e4c7aefb3e14066d5f48dcb0e3a82ef4f
MikaClaes <claesmika@gmail.com>
2025-02-20 11:33:01 +0100

    Ledtest

---
+++ ledtest.py
@@ -0,0 +1,21 @@
+import time
+import wiringpi
+
+def blink(_pin):
+    wiringpi.digitalWrite(_pin, 1)  # Write HIGH to pin
+    time.sleep(0.5)
+    wiringpi.digitalWrite(_pin, 0)  # Write LOW to pin
+    time.sleep(0.5)
+
+# SETUP
+print("Start")
+pin = 2  # Define the GPIO pin
+wiringpi.wiringPiSetup()
+wiringpi.pinMode(pin, 1)  # Set pin as OUTPUT
+
+# MAIN
+for i in range(10):
+    blink(pin)
+
+# CLEANUP
+print("Done")
