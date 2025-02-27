923d0c0948a4c648b992135ebf480b5be75fbe89
MikaClaes <claesmika@gmail.com>
2025-02-27 13:40:36 +0100

    practice 4 working

--- Lab3.2/practice4.py
+++ Lab3.2/practice4.py
@@ -4,29 +4,32 @@ import sys
 
 def blink(_pin):
     for i in range(0,3):
-        wiringpi.digitalWrite(_pin, 1)
-        time.sleep(0.5)
         wiringpi.digitalWrite(_pin, 0)
         time.sleep(0.5)
-    for i in range(0,3):
         wiringpi.digitalWrite(_pin, 1)
+        time.sleep(0.5)
+    for i in range(0,3):
+        wiringpi.digitalWrite(_pin, 0)
         time.sleep(1.5)
-        wiringpi.digitalWrite(_pin, 0)
-        time.sleep(0.5)
-    for i in range(0,3):
         wiringpi.digitalWrite(_pin, 1)
         time.sleep(0.5)
+    for i in range(0,3):
         wiringpi.digitalWrite(_pin, 0)
+        time.sleep(0.5)
+        wiringpi.digitalWrite(_pin, 1)
         time.sleep(0.5)
     time.sleep(1)
     
 
 # SETUP
 print("Start")
-pin = 2
+LED_PIN = 3
+SWITCH_PIN = 2
 wiringpi.wiringPiSetup()
-wiringpi.pinMode(pin, 1)
+wiringpi.pinMode(LED_PIN, 1)
+wiringpi.pinMode(SWITCH_PIN, 0)
 
 # MAIN
 while True:
-    blink(pin)
+    if(wiringpi.digitalRead(SWITCH_PIN) == 1):
+        blink(LED_PIN)
